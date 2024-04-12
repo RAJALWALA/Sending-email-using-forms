5079f9e923acdc6e700ff163def8f996f300506b
bunny <bunnyaluvala@gmail.com>
2024-04-12 06:38:32 -0400

    send email using forms

--- Send_email.py
+++ Send_email.py
@@ -1,3 +1,4 @@
+import os
 import smtplib,ssl
 
 
@@ -5,7 +6,7 @@ def Send_email(message):
     host = "smtp.gmail.com"
     port = 465
     user_name = "bunnyaluvala@gmail.com"
-    password = "braw qsjs wbky cqfv"
+    password = os.getenv("PASSWORD")
     recevier_email = "bunnyaluvala@gmail.com"
     context = ssl.create_default_context()
 
